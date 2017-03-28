3b293a8ee07c183e605e2e0eac9e0ca541852ebf
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-03-28 16:44:04 -0400

    Change `/` to `\` in batch build script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,7 @@ setlocal EnableDelayedExpansion
 :: This will allow them to be run on environment activation.
 FOR %%F IN (activate deactivate) DO (
     IF NOT EXIST %PREFIX%\etc\conda\%%F.d MKDIR %PREFIX%\etc\conda\%%F.d
-    COPY %RECIPE_DIR%/%%F.bat %PREFIX%\etc\conda\%%F.d\keras_%%F.bat
+    COPY %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\keras_%%F.bat
 )
 
 "%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
