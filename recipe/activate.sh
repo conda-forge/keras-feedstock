53254135d196f9d84633a3257b43cb195a237cdd
Matt Antalek <matthew.antalek@gmail.com>
2017-04-03 12:12:17 -0500

    updated the activate.sh script to read the “backend” variable set in the user defined keras.json file. Fix for mac and linux

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [ "$(uname)" == "Darwin" ]
 then
     # for Mac OSX
@@ -7,4 +8,19 @@ elif [ "$(uname)" == "Linux" ]
 then
     # for Linux
     export KERAS_BACKEND=theano
+fi
+
+# get the location of the keras.json file
+KERAS_JSON="$HOME/.keras/keras.json"
+
+if [ -f $KERAS_JSON ]; then # if the file exists
+
+  BACKEND_KEY="backend" # value we are interested in finding
+  KEY_VALUE_REGEX="\"($BACKEND_KEY)\": \"([^\"]*)\"" # regex
+  JSON_TEXT=$(cat $KERAS_JSON) # data in keras.json file
+
+  if [[ $JSON_TEXT =~ $KEY_VALUE_REGEX ]]; then # check for a match
+    BACKEND_VALUE="${BASH_REMATCH[2]}" # get the user defined backend value
+    export KERAS_BACKEND=$BACKEND_VALUE # set the environmental variable
+  fi
 fi
