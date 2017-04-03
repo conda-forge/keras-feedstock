6933ec721eb78b439ffc7de6aaf0b9d171b490df
Matt Antalek <matthew.antalek@gmail.com>
2017-04-03 15:38:14 -0500

    moved previous env setting block into else statement if keras.json file is not found

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-if [ "$(uname)" == "Darwin" ]
-then
-    # for Mac OSX
-    export KERAS_BACKEND=tensorflow
-elif [ "$(uname)" == "Linux" ]
-then
-    # for Linux
-    export KERAS_BACKEND=theano
-fi
-
 # get the location of the keras.json file
 KERAS_JSON="$HOME/.keras/keras.json"
 
@@ -22,5 +12,15 @@ if [ -f $KERAS_JSON ]; then # if the file exists
   if [[ $JSON_TEXT =~ $KEY_VALUE_REGEX ]]; then # check for a match
     BACKEND_VALUE="${BASH_REMATCH[2]}" # get the user defined backend value
     export KERAS_BACKEND=$BACKEND_VALUE # set the environmental variable
+  fi
+else
+  if [ "$(uname)" == "Darwin" ]
+  then
+      # for Mac OSX
+      export KERAS_BACKEND=tensorflow
+  elif [ "$(uname)" == "Linux" ]
+  then
+      # for Linux
+      export KERAS_BACKEND=theano
   fi
 fi
