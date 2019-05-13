58f8794e7758a9ac905c33d1b4eba76f92e33ea1
Pat Gunn <pgunn01@gmail.com>
2019-05-13 18:14:23 -0400

    another backend settings switch

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -1,13 +1,3 @@
-:: Figure out the default Keras backend by reading the config file.
-python %CONDA_PREFIX%\etc\keras\load_config.py > temp.txt
-set /p KERAS_BACKEND=<temp.txt
-del temp.txt
+set /p KERAS_BACKEND=tensorflow
 
-:: Try to use the default Keras backend.
-:: Fallback to Theano if it fails (Theano always works).
 python -c "import keras" 1> nul 2>&1
-if errorlevel 1 (
-    ver > nul
-    set "KERAS_BACKEND=tensorflow"
-    python -c "import keras" 1> nul 2>&1
-)
